656ac1a9df96d27897e392ccfaae76057ad3d07a
Mepho9 <babayev.r@outlook.com>
2022-09-28 11:29:30 +0200

    register done with asnot-backend api

--- src/app/security/components/register/register.page.ts
+++ src/app/security/components/register/register.page.ts
@@ -14,12 +14,19 @@ export class RegisterPage implements OnInit {
 
   registerForm = new FormGroup({
     sexe: new FormControl("", [Validators.required, Validators.min(3)]),
+
     username: new FormControl("", [Validators.required, Validators.min(3)]),
-
-    email: new FormControl("", [Validators.required, Validators.min(3)]),
-
     password: new  FormControl("", [Validators.required, Validators.min(4)]),
-    confirmPassword: new FormControl("", [Validators.required, Validators.min(4)])
+    confirmPassword: new FormControl("", [Validators.required, Validators.min(4)]),
+
+    pseudo: new FormControl("", [Validators.required, Validators.min(3)]),
+
+    country: new FormControl("", [Validators.required, Validators.min(3)]),
+
+    birthDate: new FormControl("", [Validators.required, Validators.min(3)]),
+
+
+
 
   });
 
@@ -33,14 +40,36 @@ export class RegisterPage implements OnInit {
 
   register(){
     if (this.registerForm.get("password")?.value === this.registerForm.get("confirmPassword")?.value) {
-      this._auth.register(<string>this.registerForm.get("username")?.value, <string>this.registerForm.get("password")?.value).subscribe(_ => {
+
+      // const $obs = this._auth.register2({
+      //   username: <string>this.registerForm.get("username")?.value,
+      //   password: <string>this.registerForm.get("password")?.value
+      // })
+      // $obs.subscribe(() =>  {/* ... */})
+
+
+      console.log( this.registerForm )
+
+      this._auth.register(
+        <string>this.registerForm.get("username")?.value,
+        <string>this.registerForm.get("password")?.value, 
+        <string>this.registerForm.get("pseudo")?.value,
+        <string>this.registerForm.get("country")?.value,
+        new Date(this.registerForm.get("birthDate")?.value)
+      ).subscribe(_ => {
         this._auth.login(<string>this.registerForm.get("username")?.value, <string>this.registerForm.get("password")?.value).subscribe(token => {
-          this.token = token["token"];
-          localStorage.setItem("token", this.token);
-          this._session.login(this.token);
-          this._router.navigate(["/home"]);
-        });
+            this.token = token["token"];
+            localStorage.setItem("token", this.token);
+            this._session.login(this.token);
+            this._router.navigate(["/home"]);
+          });
       });
     }
   }
 }
+
+// export interface RegisterForm {
+//   username: string;
+//   password: string;
+//   // ...
+// }
--- src/app/security/services/auth.service.ts
+++ src/app/security/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
+// import { RegisterForm } from '../components/register/register.page';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,14 @@ export class AuthService {
   constructor(private _http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
-    return this._http.post<any>("https://demo-rest-springboot.herokuapp.com/user/login", {"username": username, "password": password });
+    return this._http.post<any>("http://localhost:8080/user/login", {"username": username, "password": password });
   }
 
-  register(username: string, password: string): Observable<any> {
-    return this._http.post<any>("https://demo-rest-springboot.herokuapp.com/user/register", {"username": username, "password": password });
+  register(username: string, password: string, pseudo:string, country:string, birthDate:Date): Observable<any> {
+    return this._http.post<any>("http://localhost:8080/user/register", {"username": username, "password": password, "pseudo": pseudo,"country":country, "birthDate": birthDate});
   }
+
+  // register2 (  form : RegisterForm ): Observable<any>{
+  //   return of("ok");
+  // }
 }
